2d46246b2cc13842e4f19f21c49a4352b3cbb768
rpompa003 <rpompa003@student.butte.edu>
2024-09-10 20:57:56 +0000

    Finished lab 1

---
+++ Lab 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main() { 
+// "puting your ASCII art into code" ;
+  cout << "ASCII art!" ;
+  cout<<              "____" << endl;
+  cout<<  ")_________/V 0 ]__" << endl;
+  cout<<  "}             __ /" << endl;
+  cout<< "/  _)_____( r_/" << endl;
+  cout<< "I_b       I_b" << endl;
+// "I wasn't really sure how to make the ASCll art on differnt lines without using end1" ;
+}
