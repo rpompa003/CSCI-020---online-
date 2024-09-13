edeab107b88ada623a3217ec452dd352a8d0398b
rpompa003 <rpompa003@student.butte.edu>
2024-09-13 13:55:18 -0700

    Update Lab 1.cpp

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std; 
 
-/Created by: Rafael Pompa
+//Created by: Rafael Pompa
 //Created On: 9/13/2024
-//This program will examine the use of output
+//This program will show the use of output
 
 int main() { 
-//
-  cout << "ASCII art!" ;
-  cout<<             "_____" << endl;
+//output top half of dog
+  cout << "This is my ASCII art!" << endl;
+  cout<< "           _____" << endl;
   cout<<  ")_________/V 0 ]__" << endl;
+//output bottom half of dog
   cout<<  "}             __ /" << endl;
   cout<< "/  _)_____( r_/" << endl;
   cout<< "I_b       I_b" << endl;
+// I wasn't sure how to make the ASCII art on different lines without using endl
 }
