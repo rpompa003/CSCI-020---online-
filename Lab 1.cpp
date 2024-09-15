624f9b6ae4584bcff82a2a053d9e72f3d9267610
rpompa003 <rpompa003@student.butte.edu>
2024-09-15 04:41:23 +0000

    Finish with Lab: Input

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,18 +1,36 @@
-#include <iostream>
-using namespace std; 
-
 //Created by: Rafael Pompa
-//Created On: 9/13/2024
-//This program will show the use of output
+//Date: 9/14
+//this program is used to show user inputs
 
-int main() { 
-//output top half of dog
-  cout << "This is my ASCII art!" << endl;
-  cout<< "           _____" << endl;
-  cout<<  ")_________/V 0 ]__" << endl;
-//output bottom half of dog
-  cout<<  "}             __ /" << endl;
-  cout<< "/  _)_____( r_/" << endl;
-  cout<< "I_b       I_b" << endl;
-// I wasn't sure how to make the ASCII art on different lines without using endl
+#include <iostream>
+using namespace std;
+
+int main() {
+  string action = "";
+  string verb = "";
+  int hours = 0;
+  string place = "";
+  string action2 = "";
+  string action3 = "";
+//inputs first choice of action word
+cout<<"Please enter a action: ";
+  cin>>action;
+//inputs of a verb
+ cout<<"Please enter a verb that ends with ing: ";
+  cin>>verb;
+cout<<"Please enter 2 digit for hours : ";
+  cin>>hours;
+//inputs of a place
+cout<<"Please enter a place: ";
+  cin>>place;
+//inputs two choice of action word
+cout<<"Please enter a action: ";
+  cin>>action2;
+//inputs third choice of action word
+cout<<"Please enter a verb that ends with ing: ";
+  cin>>action3;
+
+cout<< "Warning! Do not " << action << " my room! I’m currently " << verb << " video games!" << "\n" ;
+cout<< "I have just spent " << hours << " hours at school, and I need some " << place << " so that I can " << action2 << " my new video game, "<< action3 << "-craft. " << "\n" ;
+//I didn't know if it is allowed to write action1, action2 and so on when the mad lib asks for more than one verb
 }
